fd4524f540da9e7b422f9c0c9498fae4c2a1d941
kyees215 <ngky1996@yahoo.com.hk>
2018-03-11 03:00:58 +0800

    Main: tab layout Search by route no. tab: upper part only

---
+++ app/src/main/java/com/hkminibus/minibus/MainActivity.java
@@ -0,0 +1,93 @@
+package com.hkminibus.minibus;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener,
+        TabLayout.OnTabSelectedListener{
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private search_by_no fragment1 = new search_by_no();
+    private search_by_location fragment2 = new search_by_location();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        viewPager.addOnPageChangeListener(this);
+        tabLayout.addOnTabSelectedListener(this);
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return fragment1;
+                    case 1:
+                        return fragment2;
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        //TabLayout里的TabItem被选中的时候触发
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        //viewPager滑动之后显示触发
+        tabLayout.getTabAt(position).select();
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
--- app/src/main/java/com/hkminibus/minibus/SplashScreen.java
+++ app/src/main/java/com/hkminibus/minibus/SplashScreen.java
@@ -14,7 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Start home activity
-        startActivity(new Intent(SplashScreen.this, test.class));
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
         // close splash activity
         finish();
     }
